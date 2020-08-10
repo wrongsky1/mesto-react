fa3fe87979cf80353f87c40f302fb47660ab3fc0
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-08-10 10:14:03 +0300

    add some components

---
+++ src/components/Main.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import api from '../utils/Api.js';
+import Card from './Card.js';
+
+function Main(props) {
+    const[userName, setUserName] = React.useState('Жак-Ив Кусто');
+    const[userDescription, setUserDescription] = React.useState('Исследователь океана');
+    const[userAvatar, setUserAvatar] = React.useState('');
+    const[cards, setCards] = React.useState([]);
+
+    React.useEffect(() => {
+        api.getUserInfo()
+          .then((data) => {
+            setUserName(data.name);
+            setUserDescription(data.about);
+            setUserAvatar(data.avatar);
+          })
+        api.getInitialCards()
+          .then((items) => {
+            setCards(items);   
+          })
+    }, []);
+
+    return(
+        <main className="content">
+            <section className="profile">
+                <img className="profile__avatar" src={userAvatar}/>
+                <div className="profile__edit-pen" onClick={props.onEditAvatar}/>
+                <h1 className="profile__info-name">{userName}</h1>
+                <button className="profile__edit-button" type="button" onClick={props.onEditProfile}/>
+                <p className="profile__info-job">{userDescription}</p>
+                <button className="profile__add-button" type="button" onClick={props.onAddPlace}/>
+            </section>
+
+            <section className="elements">
+                {cards.map((card) => <Card key={card._id} card={card} onCardDelete={props.onCardDelete} onCardClick={props.onCardClick}/>)}
+            </section>
+        </main>
+    );
+}
+
+export default Main
---
+++ src/utils/Api.js
@@ -0,0 +1,136 @@
+export default class Api {
+    constructor({ baseUrl, ownId, headers = {} }) {
+      this.baseUrl = baseUrl;
+      this.ownId = ownId;
+      this.headers = headers;
+    }
+  
+    _checkResponse(res){
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Ошибка: ${res.status}`);
+      }
+    }
+  
+    _checkResponseError(err){
+      console.log('Error');
+      return Promise.reject(err.message);
+    }
+
+    getUserInfo() {
+      return fetch(
+        `${this.baseUrl}/users/me`,
+        {
+          headers: this.headers
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    setUserInfo(userInfo) {
+      return fetch(
+        `${this.baseUrl}/users/me`,
+        {
+          method: 'PATCH',
+          headers: this.headers,
+          body: JSON.stringify({
+            name: userInfo.name,
+            about: userInfo.job 
+          })
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    changeAvatar(avatar) {
+      return fetch(
+        `${this.baseUrl}/users/me/avatar`,
+        {
+          method: 'PATCH',
+          headers: this.headers,
+          body: JSON.stringify({
+            avatar: avatar.link
+          })
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    getInitialCards() {
+      return fetch(
+        `${this.baseUrl}/cards`,
+        {
+          headers: this.headers
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    setCard(card) {
+      return fetch(
+        `${this.baseUrl}/cards`,
+        {
+          method: 'POST',
+          headers: this.headers,
+          body: JSON.stringify({
+            name: card.title,
+            link: card.link
+          })
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    setLike(id) {
+      return fetch(
+        `${this.baseUrl}/cards/likes/${id}`,
+        {
+          method: 'PUT',
+          headers: this.headers
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    deleteLike(id) {
+      return fetch(
+        `${this.baseUrl}/cards/likes/${id}`,
+        {
+          method: 'DELETE',
+          headers: this.headers
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  
+    deleteCard(id) {
+      return fetch(
+        `${this.baseUrl}/cards/${id}`,
+        {
+          method: 'DELETE',
+          headers: this.headers
+        }
+      )
+      .then(this._checkResponse)
+      .catch(this._checkResponseError)
+    }
+  }
+
+  const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-13',
+    ownId: '993c8682f39a3e6b3e598d71',
+    headers: {
+      authorization: 'c0f1ac49-a1c2-4501-916f-6ad383af4504',
+       'Content-Type': 'application/json'
+    }
+  });
+
+  export default api;
